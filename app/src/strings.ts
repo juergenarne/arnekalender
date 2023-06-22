b209ac318a1046bc20d42303692a1b45c1457b82
Juergen Arne Klein <mail@juergen-arne-klein.de>
2023-06-22 18:11:31 +0200

    [strings] add translation strings

---
+++ app/src/strings.ts
@@ -0,0 +1,10 @@
+export const strings  = {
+    mon: {
+        short: ['Jan', 'Feb', 'Mrz', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dez'],
+        long: ['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember'],
+    }, 
+    day: {
+        short: ['Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa', 'So'],
+        long: ['Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag', 'Sonntag'],
+    },
+}
